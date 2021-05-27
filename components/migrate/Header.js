320d27dc7b9198dfae853b82b7ecf8275ad9dcc2
galanggg <tugasgalang@gmail.com>
2021-05-28 07:40:19 +0800

    fix header

--- components/migrate/Header.js
+++ components/migrate/Header.js
@@ -44,25 +44,31 @@ const Header = () => {
               href="#"
               className="text-base font-medium text-gray-500 hover:text-gray-900"
             >
-              Pricing
+              Client
             </a>
             <a
               href="#"
               className="text-base font-medium text-gray-500 hover:text-gray-900"
             >
-              Partners
+              Products
             </a>
             <a
               href="#"
               className="text-base font-medium text-gray-500 hover:text-gray-900"
             >
-              Company
+              Features
+            </a>
+            <a
+              href="#"
+              className="text-base font-medium text-gray-500 hover:text-gray-900"
+            >
+              Benefits
             </a>
           </nav>
           <div className="hidden md:flex items-center justify-end md:flex-1 lg:w-0">
             <a
               className="ml-8 whitespace-nowrap cursor-pointer inline-flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-gradient-to-r from-purple-600 to-indigo-600 hover:from-purple-700 hover:to-indigo-700"
-              onClick={() => router.push('/experimentblogs', `/blogs`)}
+              onClick={() => router.push('/experimentblogs')}
             >
               Blog
             </a>
@@ -104,113 +110,6 @@ const Header = () => {
                   </button>
                 </div>
               </div>
-              <div className="mt-6">
-                <nav className="grid grid-cols-1 gap-7">
-                  <a
-                    href="#"
-                    className="-m-3 p-3 flex items-center rounded-lg hover:bg-gray-50"
-                  >
-                    <div className="flex-shrink-0 flex items-center justify-center h-10 w-10 rounded-md bg-gradient-to-r from-purple-600 to-indigo-600 text-white">
-                      <svg
-                        className="h-6 w-6"
-                        xmlns="http://www.w3.org/2000/svg"
-                        fill="none"
-                        viewBox="0 0 24 24"
-                        stroke="currentColor"
-                        aria-hidden="true"
-                      >
-                        <path
-                          strokeLinecap="round"
-                          strokeLinejoin="round"
-                          strokeWidth="2"
-                          d="M20 13V6a2 2 0 00-2-2H6a2 2 0 00-2 2v7m16 0v5a2 2 0 01-2 2H6a2 2 0 01-2-2v-5m16 0h-2.586a1 1 0 00-.707.293l-2.414 2.414a1 1 0 01-.707.293h-3.172a1 1 0 01-.707-.293l-2.414-2.414A1 1 0 006.586 13H4"
-                        />
-                      </svg>
-                    </div>
-                    <div className="ml-4 text-base font-medium text-gray-900">
-                      Inbox
-                    </div>
-                  </a>
-
-                  <a
-                    href="#"
-                    className="-m-3 p-3 flex items-center rounded-lg hover:bg-gray-50"
-                  >
-                    <div className="flex-shrink-0 flex items-center justify-center h-10 w-10 rounded-md bg-gradient-to-r from-purple-600 to-indigo-600 text-white">
-                      <svg
-                        className="h-6 w-6"
-                        xmlns="http://www.w3.org/2000/svg"
-                        fill="none"
-                        viewBox="0 0 24 24"
-                        stroke="currentColor"
-                        aria-hidden="true"
-                      >
-                        <path
-                          strokeLinecap="round"
-                          strokeLinejoin="round"
-                          strokeWidth="2"
-                          d="M7 8h10M7 12h4m1 8l-4-4H5a2 2 0 01-2-2V6a2 2 0 012-2h14a2 2 0 012 2v8a2 2 0 01-2 2h-3l-4 4z"
-                        />
-                      </svg>
-                    </div>
-                    <div className="ml-4 text-base font-medium text-gray-900">
-                      Messaging
-                    </div>
-                  </a>
-
-                  <a
-                    href="#"
-                    className="-m-3 p-3 flex items-center rounded-lg hover:bg-gray-50"
-                  >
-                    <div className="flex-shrink-0 flex items-center justify-center h-10 w-10 rounded-md bg-gradient-to-r from-purple-600 to-indigo-600 text-white">
-                      <svg
-                        className="h-6 w-6"
-                        xmlns="http://www.w3.org/2000/svg"
-                        fill="none"
-                        viewBox="0 0 24 24"
-                        stroke="currentColor"
-                        aria-hidden="true"
-                      >
-                        <path
-                          strokeLinecap="round"
-                          strokeLinejoin="round"
-                          strokeWidth="2"
-                          d="M17 8h2a2 2 0 012 2v6a2 2 0 01-2 2h-2v4l-4-4H9a1.994 1.994 0 01-1.414-.586m0 0L11 14h4a2 2 0 002-2V6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2v4l.586-.586z"
-                        />
-                      </svg>
-                    </div>
-                    <div className="ml-4 text-base font-medium text-gray-900">
-                      Live Chat
-                    </div>
-                  </a>
-
-                  <a
-                    href="#"
-                    className="-m-3 p-3 flex items-center rounded-lg hover:bg-gray-50"
-                  >
-                    <div className="flex-shrink-0 flex items-center justify-center h-10 w-10 rounded-md bg-gradient-to-r from-purple-600 to-indigo-600 text-white">
-                      <svg
-                        className="h-6 w-6"
-                        xmlns="http://www.w3.org/2000/svg"
-                        fill="none"
-                        viewBox="0 0 24 24"
-                        stroke="currentColor"
-                        aria-hidden="true"
-                      >
-                        <path
-                          strokeLinecap="round"
-                          strokeLinejoin="round"
-                          strokeWidth="2"
-                          d="M8.228 9c.549-1.165 2.03-2 3.772-2 2.21 0 4 1.343 4 3 0 1.4-1.278 2.575-3.006 2.907-.542.104-.994.54-.994 1.093m0 3h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
-                        />
-                      </svg>
-                    </div>
-                    <div className="ml-4 text-base font-medium text-gray-900">
-                      Knowledge Base
-                    </div>
-                  </a>
-                </nav>
-              </div>
             </div>
             <div className="py-6 px-5">
               <div className="grid grid-cols-2 gap-4">
@@ -218,19 +117,19 @@ const Header = () => {
                   href="#"
                   className="text-base font-medium text-gray-900 hover:text-gray-700"
                 >
-                  Pricing
+                  Client
                 </a>
                 <a
                   href="#"
                   className="text-base font-medium text-gray-900 hover:text-gray-700"
                 >
-                  Partners
+                  Products
                 </a>
                 <a
                   href="#"
                   className="text-base font-medium text-gray-900 hover:text-gray-700"
                 >
-                  Company
+                  Features
                 </a>
               </div>
               <div className="mt-6">
@@ -238,14 +137,8 @@ const Header = () => {
                   href="#"
                   className="w-full flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-gradient-to-r from-purple-600 to-indigo-600 hover:from-purple-700 hover:to-indigo-700"
                 >
-                  Sign up
+                  Blogs
                 </a>
-                <p className="mt-6 text-center text-base font-medium text-gray-500">
-                  Existing customer?
-                  <a href="#" className="text-gray-900">
-                    Sign in
-                  </a>
-                </p>
               </div>
             </div>
           </div>
